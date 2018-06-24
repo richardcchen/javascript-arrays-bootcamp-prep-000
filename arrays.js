2d0ee4add04565f7638ffc334d4af39c5869d919
richard.c.chen@gmail.com <richard.c.chen@gmail.com>
2018-06-24 16:37:09 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, additional){
+  return [additional,...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, additional){
+  return arr.unshift(additional)
+  
+}
